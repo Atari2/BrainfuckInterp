6fdd75b23ff24c0afaab2b26055eb1c698d32f3c
Atari2 <alex.robassomero@gmail.com>
2021-10-19 10:50:30 +0200

    Refactor code to be much nicer overall. Swap to C++20.

--- BrainfuckInterp/main.cpp
+++ BrainfuckInterp/main.cpp
@@ -3,21 +3,24 @@
 #include <cstdlib>
 #include <stack>
 
-#ifndef CUSTOM_CELL_SIZE
+#ifndef CUSTOM_CELL_SIZE	// cell size in bits, valid values are 8, 16, 32, 64
 #define CELL_SIZE 8
 #endif
 
-#ifndef CUSTOM_CELL_WRAP_BEHAVIOR
+#ifndef CUSTOM_CELL_WRAP_BEHAVIOR	// wrapping for cell, 1 for wrap, 0 for not wrap.
 #define CELL_WRAP 1
 #endif
 
 using size_t = decltype(sizeof(void*));
+using funcptr = int(*)(int, size_t&, char*);
 
 #if CELL_SIZE == 8
 #if CELL_WRAP == 1
 using cell_type = int8_t;
+#define PRINT_C "%c"
 #else
 using cell_type = uint8_t;
+#define PRINT_C "%hhu"
 #endif
 #elif CELL_SIZE == 16
 #if CELL_WRAP == 1
@@ -47,31 +50,48 @@ using cell_type = uint64_t;
 #error "Cell sizes supported are 8, 16, 32, 64 bytes"
 #endif
 
-template <typename T, size_t N>
-constexpr size_t sizeof_array(const T(&val)[N]) {
-	return N;
-}
+// recommended size of buffer_size apparently
+// from esolangs.org
+constexpr size_t buffer_size = 30'000;
 
-int do_action(char c, int current_index, cell_type* execution_buffer, size_t& text_index, char* text_buffer, std::stack<size_t>& parens) {
-	switch (c) {
-	case '>':
-		return current_index + 1;
-	case '<':
-		return current_index - 1;
-	case '+':
-		++execution_buffer[current_index];
-		return current_index;
-	case '-':
-		--execution_buffer[current_index];
-		return current_index;
-	case '.':
-#if CELL_SIZE == 8
-#if '\n' == 10
-		putchar(execution_buffer[current_index]);
-#else
-		putchar(execution_buffer[current_index] == 10 ? '\n' : execution_buffer[current_index]);
-#endif
-#else
+// global state
+static cell_type execution_buffer[buffer_size] = { 0 };
+static std::stack<size_t> parens{};
+
+struct fn_pair {
+	char c;
+	funcptr fn;
+};
+
+int action_increment_index(int current_index, size_t&, char*) {
+	// >
+	return current_index + 1;
+}
+int action_decrement_index(int current_index, size_t&, char*) {
+	// <
+	return current_index - 1;
+}
+int action_increment_cell(int current_index, size_t&, char*) {
+	// +
+	++execution_buffer[current_index];
+	return current_index;
+}
+int action_decrement_cell(int current_index, size_t&, char*) {
+	// -
+	--execution_buffer[current_index];
+	return current_index;
+}
+int action_print_cell(int current_index, size_t&, char*) {
+	// .
+	if constexpr (CELL_SIZE == 8) {
+		if constexpr ('\n' == 10) {
+			putchar(execution_buffer[current_index]);
+		}
+		else {
+			putchar(execution_buffer[current_index] == 10 ? '\n' : execution_buffer[current_index]);
+		}
+	}
+	else {
 		// for cell sizes that are not 8 it doesn't make sense to print a char
 		if (cell_type{ '\n' } == execution_buffer[current_index]) {
 			putchar('\n');
@@ -79,58 +99,63 @@ int do_action(char c, int current_index, cell_type* execution_buffer, size_t& te
 		else {
 			printf(PRINT_C, execution_buffer[current_index]);
 		}
-#endif
-		return current_index;
-	case ',': {
-		// for input we always read a char regardless
-#if '\n' == 10
+	}
+	return current_index;
+}
+
+int action_input_cell(int current_index, size_t&, char*) {
+	// ,
+	// for input we always read a char regardless
+	if constexpr ('\n' == 10) {
 		execution_buffer[current_index] = static_cast<cell_type>(getchar());
-#else
+	}
+	else {
 		char nc = getchar();
 		execution_buffer[current_index] = (nc == '\n') ? 10 : nc;
-#endif
-		return current_index;
 	}
-	case '[':
-		parens.push(text_index);
-		return current_index;
-	case ']': {
-		if (parens.empty()) {
-			printf("Unbalanced []\n");
-			exit(EXIT_FAILURE);
-		}
-		if (execution_buffer[current_index] != 0) {
-			text_index = parens.top();
-		}
-		else {
-			parens.pop();
-		}
-		return current_index;
+	return current_index;
+}
+
+int action_loop_open(int current_index, size_t& text_index, char* text_buffer) {
+	// [
+	parens.push(text_index);
+	return current_index;
+}
+int action_loop_close(int current_index, size_t& text_index, char*) {
+	// ]
+	if (parens.empty()) {
+		printf("Unbalanced []\n");
+		exit(EXIT_FAILURE);
 	}
-	default:
-		return current_index;
+	if (execution_buffer[current_index] != 0) {
+		text_index = parens.top();
 	}
+	else {
+		parens.pop();
+	}
+	return current_index;
 }
 
 int main(int argc, char* argv[]) {
-	constexpr char keychars[] = { '>', '<', '+', '-', '.', ',', '[', ']' };
+	static constexpr fn_pair keychars[] = {
+		{'>', action_increment_index},
+		{'<', action_decrement_index},
+		{'+', action_increment_cell},
+		{'-', action_decrement_cell},
+		{'.', action_print_cell},
+		{',', action_input_cell},
+		{'[', action_loop_open},
+		{']', action_loop_close}
+	};
 
-	std::stack<size_t> parens{};
-
-	using funcptr = decltype(&do_action);
-
-	auto check_char = [&keychars](char c) -> funcptr {
-		for (size_t i = 0; i < sizeof_array(keychars); i++) {
-			if (c == keychars[i]) return &do_action;
+	auto check_char = [](char c) -> funcptr {
+		for (const auto& [kc, fn] : keychars) {
+			if (c == kc) return fn;
 		}
 		return nullptr;
 	};
+
 	char filename_buffer[FILENAME_MAX + 1] = { 0 };
-	// recommended size of buffer_size apparently
-	// from esolangs.org
-	constexpr size_t buffer_size = 30'000;
-	cell_type* execution_buffer = new cell_type[buffer_size];
-	memset(execution_buffer, 0, buffer_size);
 	int current_buffer_index = 0;
 	size_t text_index = 0;
 	if (argc > 1) {
@@ -158,7 +183,7 @@ int main(int argc, char* argv[]) {
 		char c = text[text_index++];
 		auto* f = check_char(c);
 		if (f) {
-			current_buffer_index = f(c, current_buffer_index, execution_buffer, text_index, text, parens);
+			current_buffer_index = f(current_buffer_index, text_index, text);
 		}
 		// wrapping behavior
 		if (current_buffer_index < 0)
@@ -168,6 +193,5 @@ int main(int argc, char* argv[]) {
 		}
 	}
 	delete[] text;
-	delete[] execution_buffer;
 	return EXIT_SUCCESS;
 }
